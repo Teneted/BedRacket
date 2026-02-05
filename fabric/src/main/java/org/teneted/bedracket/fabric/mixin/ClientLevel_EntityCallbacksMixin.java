52b408385667fe98275c04bd1ebd89a632653a5b
wdog5734 <1798643961@qq.com>
2026-02-05 16:18:39 +0800

    fix missing annotations

--- fabric/src/main/java/org/teneted/bedracket/fabric/mixin/ClientLevel_EntityCallbacksMixin.java
+++ fabric/src/main/java/org/teneted/bedracket/fabric/mixin/ClientLevel_EntityCallbacksMixin.java
@@ -4,6 +4,7 @@ import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,6 +12,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.teneted.bedracket.BedRacket;
 import org.teneted.bedracket.event.entity.EntityRemoveFromWorldEvent;
 
+@Pseudo
 @Mixin(targets = "net.minecraft.client.multiplayer.ClientLevel.EntityCallbacks")
 public class ClientLevel_EntityCallbacksMixin {
 
